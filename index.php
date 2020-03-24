04afd8af2eb93bce82dd94a8d2450c59756f5f87
suttipongJJ <59908944+suttipongJJ@users.noreply.github.com>
2020-03-24 09:58:34 +0700

    sutinew

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "62310072"; ?><br>
+<?php echo "ชื่อ-สกุล นายสุทธิพงษ์ ชมภูฒาศ"; ?><br>
+<iframe width="500" height="500"
+src="https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1">
+</iframe>
+</BODY>
+</HTML>
